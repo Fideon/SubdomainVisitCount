1f81d49e51ffa5e809d73149815757b5bd5b8146
Fidel Salazar <fidelsf91@gmail.com>
2022-03-27 22:02:49 -0500

    Solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string[] cpdomains = new string[] { "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org" };
+
+        solution.SubdomainVisits(cpdomains);
+    }
+
+    public IList<string> SubdomainVisits(string[] cpdomains)
+    {
+        Dictionary<string, int> counts = new Dictionary<string, int>();
+        foreach (string cpdomain in cpdomains)
+        {
+            string[] splitcp = cpdomain.Split(' ');
+            string domain = splitcp[1];
+            int visits = Int32.Parse(splitcp[0]);
+            string[] subdomains = domain.Split('.');
+
+
+            for (int i = 0; i < subdomains.Count(); i++)
+            {
+                if (!counts.ContainsKey(domain))
+                {
+                    counts.Add(domain, 0);
+                }
+
+                counts[domain] += visits;
+
+                if (i < subdomains.Count() - 1)
+                {
+                    domain = domain.Replace(subdomains[i], "");
+                    domain = domain.Remove(0, 1);
+                }
+            }
+        }
+
+        List<String> result = new List<String>();
+
+        foreach (KeyValuePair<string, int> count in counts)
+        {
+            string c = count.Value.ToString();
+            result.Add($"{c} {count.Key}");
+        }
+
+        return result;
+    }
+}
